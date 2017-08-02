4a4dfa3848e81c30b88dcf2eb647ea2beca2c3f0
Yves Ramos <ymramos08@gmail.com>
2017-08-01 20:42:29 -0400

    added theme, change color and added photos.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery.easing
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
