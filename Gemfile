d3154afee1284c9e62f27f250a22088c537ed133
Yves Ramos <ymramos08@gmail.com>
2017-08-05 12:29:48 -0400

    Add tables Post for photos and category to divide the photos by category later on. Tested and now I can upload photos

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'better_errors'
 gem 'devise'
 gem 'carrierwave'
 
+gem "paperclip", "~> 4.2"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :categories
+  resources :posts
+
   root 'home#index'
+
+  get 'post' => 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
