d99a87b9ef01395f5161b5f442a37173b35d58fb
Yves Ramos <ymramos08@gmail.com>
2017-08-06 22:45:35 -0400

    check ToDo file

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   resources :categories
   resources :posts
 
+  # root 'home#index'
   root 'home#index'
 
-  get 'post' => 'posts#index'
+  get 'portfolio' => 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
