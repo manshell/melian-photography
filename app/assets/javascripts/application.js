1dfd2bf7dd572b0aadc27b924777b10af042b96c
Yves Ramos <ymramos08@gmail.com>
2017-08-10 10:23:33 -0400

    Pushing to Heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require turbolinks
 //= require jquery
 //= require jquery.easing
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require creative
 //= require_tree .
 
