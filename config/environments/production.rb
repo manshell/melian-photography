451da53512bf41c9466023112a87402b83040fc0
Yves Ramos <ymramos08@gmail.com>
2017-08-15 11:48:52 -0400

    changed config.assets.compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ config.paperclip_defaults = {
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
