2b26a708ddcb4b470c330cba89c7b920b8b84826
Yves Ramos <ymramos08@gmail.com>
2017-08-16 00:52:10 -0400

    Moved all JS to the creative js

--- app/assets/javascripts/creative.js
+++ app/assets/javascripts/creative.js
@@ -1,6 +1,28 @@
 (function($) {
     "use strict"; // Start of use strict
 
+    //Navbar background will turn white
+$(document).on('turbolinks:load', function() {
+// Offset for Main Navigation
+$('#mainNav').affix({
+    offset: {
+        top: 50
+    }
+});
+
+$('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+
+//On click image will get bigger.
+
+$("#button-transform").click(function() {
+  $('.transform').toggleClass('transform-active');
+});
+});
+
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
@@ -10,11 +32,7 @@
         event.preventDefault();
     });
 
-    // Highlight the top nav as scrolling occurs
-    // $('body').scrollspy({
-    //     target: '.navbar-fixed-top',
-    //     offset: 51
-    // });
+
 
     $(function() {
       $('a.page-scroll').bind('click', function(event) {
@@ -25,46 +43,5 @@
       event.preventDefault();
     });
   });
-
-
-// Not Using*****************
-
-    // // Closes the Responsive Menu on Menu Item Click
-    // $('.navbar-collapse ul li a').click(function() {
-    //     $('.navbar-toggle:visible').click();
-    // });
-
-    // Initialize and Configure Scroll Reveal Animation
-    // window.sr = ScrollReveal();
-    // sr.reveal('.sr-icons', {
-    //     duration: 600,
-    //     scale: 0.3,
-    //     distance: '0px'
-    // }, 200);
-    // sr.reveal('.sr-button', {
-    //     duration: 1000,
-    //     delay: 200
-    // });
-    // sr.reveal('.sr-contact', {
-    //     duration: 600,
-    //     scale: 0.3,
-    //     distance: '0px'
-    // }, 300);
-
-    // // Initialize and Configure Magnific Popup Lightbox Plugin
-    // $('.popup-gallery').magnificPopup({
-    //     delegate: 'a',
-    //     type: 'image',
-    //     tLoading: 'Loading image #%curr%...',
-    //     mainClass: 'mfp-img-mobile',
-    //     gallery: {
-    //         enabled: true,
-    //         navigateByImgClick: true,
-    //         preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-    //     },
-    //     image: {
-    //         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    //     }
-    // });
 
 })(jQuery); // End of use strict
