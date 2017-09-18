8b740e393f9c5eaa49375b38e9f7ad53f541a09d
Yves Ramos <ymramos08@gmail.com>
2017-09-18 10:53:09 -0400

    Some bug fixed, picture were not the same size in portfolio

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,7 +11,13 @@ class HomeController < ApplicationController
 
   	@posts = Post.where(category_id: params[:cat_id])
   	@category = Category.find(params[:cat_id])
+
   end
+
+  # def cat_one
+  #   @posts = Post.where(category_id: 1)
+  #   @category = Category.find(1)
+  # end
 
   def admin
 
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
     get 'admin', to: 'devise/sessions#new'
   end
 
+  # get 'cat_one' => 'home#cat_one'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
